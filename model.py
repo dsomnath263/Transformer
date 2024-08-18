aad5d3c444b67f9fc77dddfc5035dfe30363a6e8
Somnath Das <dsomnath263@gmail.com>
2024-08-19 02:38:49 +0530

    ok

--- model.py
+++ model.py
@@ -141,6 +141,7 @@ class DecoderBlock(nn.Module):
     def forward(self,x,encoder_output,src_mask,tgt_mask):
         x=self.residual_connection[0](x,lambda x:self.self_attention_block(x,x,x,tgt_mask))
         x=self.residual_connection[1](x,lambda x: self.cross_attention_block(x,encoder_output,encoder_output,src_mask))
+        x = self.residual_connection[2](x, self.feed_forward_block)
         return x
 class Decoder(nn.Module):
     def __init__(self,layers: nn.ModuleList)->None:
